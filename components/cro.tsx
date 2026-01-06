f48a687f5cbf6afcee2608514a9d99328629f1a2
erika-goodwin <shellvoerika@gmail.com>
2026-01-06 16:51:51 +0100

    Done | hide CRO section

--- components/cro.tsx
+++ components/cro.tsx
@@ -14,9 +14,10 @@ export default function Cro() {
 
       <h2 className="hidden md:inline-block text-2xl font-bold mb-4">CRO</h2>
       <div className="space-y-4">
-        {croProjects.map((project) => (
+        {/* {croProjects.map((project) => (
           <ProjectTile key={project.id} project={project} />
-        ))}
+        ))} */}
+        Preparing ... 
       </div>
       {/* <div className="flex flex-row items-center">
         <p className="mr-4 font-bold">View More </p>
