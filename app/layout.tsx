f64386f20f8174ba2c3d21c6262125520e87206c
erika-goodwin <shellvoerika@gmail.com>
2026-01-07 17:28:00 +0100

    add | favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
-// import { Geist, Geist_Mono, Rubik } from "next/font/google";
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 const rubik = Rubik({
   variable: "--font-rubik",
@@ -31,12 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${rubik.variable} antialiased`}
-        // className={`${geistSans.variable} ${geistMono.variable} ${rubik.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${rubik.variable} antialiased`}>{children}</body>
     </html>
   );
 }
